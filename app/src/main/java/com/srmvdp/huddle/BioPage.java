577c3abf08d7655fea3114d01e7dad1025db3255
Sagar Vakkala <sagarwakawaka@gmail.com>
2016-10-22 16:47:01 +0530

    Finished BioPage xml

--- app/src/main/java/com/srmvdp/huddle/BioPage.java
+++ app/src/main/java/com/srmvdp/huddle/BioPage.java
@@ -1,9 +1,11 @@
 package com.srmvdp.huddle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
+import android.view.MenuItem;
 
 public class BioPage extends AppCompatActivity {
 
@@ -18,7 +20,21 @@ public class BioPage extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         getSupportActionBar().setTitle(Html.fromHtml("<font color='#000000'>Profile page</font>"));
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                Intent intent = new Intent(this, Dashboard.class);
+                finish();
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
