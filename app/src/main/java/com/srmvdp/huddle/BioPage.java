9566a1fbdf19de3ae2beff3894554121a35b2c80
Sagar Vakkala <sagarwakawaka@gmail.com>
2016-10-23 01:09:05 +0530

    50% completion of profile edit page

--- app/src/main/java/com/srmvdp/huddle/BioPage.java
+++ app/src/main/java/com/srmvdp/huddle/BioPage.java
@@ -6,10 +6,15 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class BioPage extends AppCompatActivity {
 
     Toolbar toolbar;
+
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,16 @@ public class BioPage extends AppCompatActivity {
         setContentView(R.layout.activity_bio_page);
 
         toolbar = (Toolbar) findViewById(R.id.toolbarbio);
+
+        button = (Button) findViewById(R.id.buttonedit);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(BioPage.this,SelfProfile.class);
+                startActivity(in);
+            }
+        });
 
         setSupportActionBar(toolbar);
 
--- app/src/main/java/com/srmvdp/huddle/SelfProfile.java
+++ app/src/main/java/com/srmvdp/huddle/SelfProfile.java
@@ -1,13 +1,39 @@
 package com.srmvdp.huddle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.view.MenuItem;
 
 public class SelfProfile extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_self_profile);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbarselfprofile);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(Html.fromHtml("<font color='#000000'>Edit Profile</font>"));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                Intent intent = new Intent(this, BioPage.class);
+                finish();
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
